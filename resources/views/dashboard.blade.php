76010eabcd20e0253dc41779865bee1a8433488e
SHIO777 <shiotatsu1015@gmail.com>
2022-09-02 20:22:44 +0900

    create table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Dashboard!!!!!') }}
+            {{ __("hello")}}
         </h2>
     </x-slot>
 
@@ -10,6 +11,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
+                    <br>
                     Hello, user!
                 </div>
             </div>
