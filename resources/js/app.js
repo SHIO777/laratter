f6f7989b59dbec159aca03d68972087682d9480a
SHIO777 <shiotatsu1015@gmail.com>
2022-09-09 19:06:47 +0900

    [delete] alert function

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,13 +5,3 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
-
-// 2022.09.09  add 削除前のalert機能
-// https://qol-kk.com/wp2/blog/2019/03/28/post-1174/
-function delete_alert(e) {
-    if (!window.confirm('本当に削除しますか？')) {
-        window.alert('キャンセルされました');
-        return false;
-    }
-    document.deleteform.submit();
-};
